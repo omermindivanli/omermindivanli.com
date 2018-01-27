5f421b04ed65000177e1c9b7378548e1e3e2c651
Ömer Mindivanli <omer.mindivanli@gmail.com>
2018-01-28 02:10:24 +0300

    Readme.md some info

--- __tests__/Portfolio.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Portfolio from '../src/Components/Portfolio/Portfolio';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Portfolio />, div);
-});
