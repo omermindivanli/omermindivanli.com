5bc02f275f7be94eac70d69276775de73dae836e
Ömer Mindivanli <omer.mindivanli@gmail.com>
2018-03-18 22:37:13 +0300

    Project fully changed from my starting page to a new boilerplate

--- client/__tests__/SocialMedia.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SocialMedia from '../src/Components/SocialMedia/SocialMedia';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<SocialMedia />, div);
-});
--- client/__tests__/Software.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Software from '../src/Components/Software/Software';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Software />, div);
-});
